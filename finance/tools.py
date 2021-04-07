a8433082a2c0a94934c437712bc5cc265eb57f8d
DavidKimDY <kdaeyoub@gmail.com>
2021-04-07 15:09:08 +0900

    Fix Error : Wrong string in finance.per() <em class =\"up\"></em>

--- finance/tools.py
+++ finance/tools.py
@@ -7,13 +7,18 @@ stock_code_list = data_reader('12021', market='전체', search_type='전종목')
 
 def get(stock='all', start=None, end=None):
     """
-    :param stock: 종목명 또는 종목코드, 기본값은 'all'
-        'all' 은 전종목 시세
-    :param start: 시작일 또는 조회일자
-        검색 시작일 또는 조회일자
-    :param end: 종료일
-        검색 종료일
-    :return: DataFrame or None
+    코스피(KOSPI), 코스닥(KOSDAQ), 코넥스(KONEX)에 상장되어 있는 종목들에 대한 가격 데이터를 반환한다.
+    Parameters
+    ----------
+    stock : string
+        종목명 또는 종목코드를 입력. default 값은 "all"이며 전종목 시세를 반환한다.
+    start : int, string
+        검색 시작일, default 값은 오늘로부터 60일 이전
+    end : int, string
+        검색 종료일, default 값은 오늘
+
+    Returns : DataFrame
+    -------
     """
     if stock == 'all':
         return data_reader('12001', market='전체', day=start)
@@ -24,20 +29,43 @@ def get(stock='all', start=None, end=None):
             return data_reader('12003', start=start, end=end, item=stock)
 
 
-def per(stock='all', start=None, end=None):
+def per(stock="all", start=None, end=None):
     """
-    :param stock: 종목명 또는 종목코드, 기본값은 'all'
-        'all' 은 전종목 검색
-    :param start: 시작일 또는 조회일자
-        검색 시작일
-    :param end: 종료일
-        검색 종료
-    :return: DataFrame or None
+    코스피(KOSPI), 코스닥(KOSDAQ), 코넥스(KONEX)에 상장되어 있는 종목들에 대한
+    PER/EPS/PBS/BPS/주당배당금/배당수익률 데이터를 반환한다.
+    Parameters
+    ----------
+    stock : string
+        종목명 또는 종목코드를 입력. default 값은 "all"이며 전종목 시세를 반환한다.
+    start : int, string
+        검색 시작일, default 값은 오늘로부터 60일 이전
+    end : int, string
+        검색 종료일, default 값은 오늘
+
+    Returns : DataFrame
+    -------
     """
     if stock == 'all':
-        return data_reader('12021', search_type='전종목', market='전체', day=start)
+        data = data_reader('12021', search_type='전종목', market='전체', day=start)
+        #  12021 종목명 데이터에 아래와 같은 문자열이 함께 출력됨.
+        data['종목명'] = [name.replace('<em class =\"up\"></em>', '') for name in data['종목명']]
+        return data
     else:
         if stock in stock_code_list:
             return data_reader('12021', search_type='개별추이', item_code=stock, start=start, end=end)
         else:
             return data_reader('12021', search_type='개별추이', item=stock, start=start, end=end)
+
+
+def etf(item="all", start=None, end=None):
+    """
+    Parameters
+    ----------
+    item
+    start
+    end
+
+    Returns
+    -------
+
+    """

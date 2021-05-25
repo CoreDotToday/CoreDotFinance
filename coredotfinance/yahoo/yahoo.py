52e289e66cb960d5f1f749d1bf737b47d42327ff
jourmooney <gukmoonahn@gmail.com>
2021-05-25 14:28:14 +0900

    update yahoo finance

--- coredotfinance/yahoo/yahoo.py
+++ coredotfinance/yahoo/yahoo.py
@@ -1,6 +1,10 @@
-import requests
 import pandas as pd
-from coredotfinance._utils import _convert_date2timestamp, _convert_timestamp2datetime_list, _get_today, _get_past_days_ago
+import requests
+from coredotfinance._utils import (
+    _convert_date2timestamp,
+    _convert_timestamp2datetime_list,
+    _get_today,
+)
 
 
 def request_get_data(ticker, start_timestamp, end_timestamp):
@@ -20,10 +24,16 @@ def request_get_data(ticker, start_timestamp, end_timestamp):
     return response.json()
 
 
-def get_ohlcv(ticker, start=_get_past_days_ago(), end=_get_today()):
+def get_ohlcv(ticker, *, start=None, end=None, adjust_price=True, real_price=False):
+    if start is None:
+        start = "19000101"
+    if end is None:
+        end = _get_today()
+
     start_stamp = _convert_date2timestamp(start)
     end_stamp = _convert_date2timestamp(end)
     response = request_get_data(ticker, start_stamp, end_stamp)
+
     timestamp = response["chart"]["result"][0]["timestamp"]
     datetime = _convert_timestamp2datetime_list(timestamp)
     open = response["chart"]["result"][0]["indicators"]["quote"][0]["open"]
@@ -47,7 +57,37 @@ def get_ohlcv(ticker, start=_get_past_days_ago(), end=_get_today()):
         .set_index("일자")
         .sort_index(ascending=False)
     )
+
+    if adjust_price:
+        df = apply_adjust_price(df)
+    elif real_price:
+        df = apply_real_price(df)
+
     return df
 
 
-print(get_ohlcv("^KS11"))
+def apply_adjust_price(data: pd.DataFrame) -> pd.DataFrame:
+    df = data.copy()
+    ratio = df["종가"] / df["수정종가"]
+    df["수정시가"] = df["시가"] / ratio
+    df["수정고가"] = df["고가"] / ratio
+    df["수정저가"] = df["저가"] / ratio
+    df["수정거래량"] = df["거래량"] * ratio
+
+    df = df.drop(["시가", "고가", "저가", "종가", "거래량"], axis=1)
+
+    df.rename(
+        columns={"수정시가": "시가", "수정고가": "고가", "수정저가": "저가", "수정종가": "종가", "수정거래량": "거래량"},
+        inplace=True,
+    )
+
+    df = df[["시가", "고가", "저가", "종가", "거래량"]]
+    return df[["시가", "고가", "저가", "종가", "거래량"]]
+
+
+def apply_real_price(data: pd.DataFrame) -> pd.DataFrame:
+    df = data.copy()
+    df = df.drop(["수정종가"], axis=1)
+
+    df = df[["시가", "고가", "저가", "종가", "거래량"]]
+    return df[["시가", "고가", "저가", "종가", "거래량"]]

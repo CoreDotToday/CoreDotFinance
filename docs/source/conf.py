518a60c34b7438985c0ff6db0ba7b57849042072
DavidKimDY <kdaeyoub@gmail.com>
2021-06-10 16:19:44 +0900

    Edit : deleted docs and restart docs using sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,16 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'CoreDotFinance'
-copyright = '2021, Core.Today'
-author = 'Core.Today'
+copyright = '2021, core.today'
+author = 'core.today'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
@@ -31,12 +31,6 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_panels',
-    'sphinx.ext.extlinks',
-    'sphinx.ext.autosummary'
-
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,7 +46,7 @@ language = 'kr'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -60,12 +54,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
-html_theme_options = {
-    'github_url': 'https://github.com/DavidKimDY/finance'
-}
+html_theme = 'alabaster'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = './_static/coredottoday_logo.jpg'

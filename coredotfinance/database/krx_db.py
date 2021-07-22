e4d6d7357690d04631bf4cbb64344698a9787916
DavidKimDY <kdaeyoub@gmail.com>
2021-07-22 15:40:46 +0900

    Add : docs

--- coredotfinance/database/krx_db.py
+++ coredotfinance/database/krx_db.py
@@ -2,15 +2,15 @@ import os
 import requests
 
 import pandas as pd
-import numpy as np
 
 __url = 'http://15.165.18.200:8080/'
+
 
 def _requests_with_retry(url):
     for _ in range(5):
         try:
             retry = False
-            response = requests.get(url, timeout = 15).json()
+            response = requests.get(url, timeout=15).json()
         except requests.exceptions.ReadTimeout:
             retry = True
             response = {'msg': 'Error : requests.exceptions.ReadTimeout'}
@@ -22,6 +22,30 @@ def _requests_with_retry(url):
 
 
 def read(symbol, start, end, kind, resource, api_key):
+    """
+
+    Parameters
+    ----------
+    symbol : str
+        조회하고자 하는 데이터의 종목코드.
+        형태는 종목과 종류마다 다르다. 예) 삼성전자 : '005930', ARIRANG 200 : '152100'
+    kind : str, default "stock"
+        조회하고자 하는 데이터의 종류.
+    resource : str
+        url 주소를 완성하기 위한 것으로 데이터의 출처값을 있다.
+    api_key : str
+        api key
+    start : str
+        조회하고자 하는 데이터의 시작일.
+        형태는 YYYY-MM-DD가 되어야 한다. 예) 2021-06-01
+    end : str
+        조회하고자 하는 데이터의 종료일.
+        형태는 YYYY-MM-DD가 되어야 한다. 예) 2021-06-01
+
+    Returns
+    -------
+    pd.DataFrame
+    """
 
     url = os.path.join(__url, resource, kind, 'read', f'?symbol={symbol}&start={start}&end={end}&apikey={api_key}')
     response = _requests_with_retry(url)

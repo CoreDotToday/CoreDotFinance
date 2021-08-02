63eb262991b07dbb315ff492e6ce3c4f881db667
DavidKimDY <kdaeyoub@gmail.com>
2021-08-02 10:23:30 +0900

    Edit : so as to make options about adjust, kor available for multi-index dataframe, etf

--- coredotfinance/krx/core/option.py
+++ coredotfinance/krx/core/option.py
@@ -5,38 +5,37 @@ import os
 
 import pandas as pd
 
-from coredotfinance.krx.core import column
+def get_shares_volume_column(columns):
 
+    for shares_column in ['상장주식수', '상장좌수', "shares_outstanding"]:
+        if shares_column in columns:
+            shares = shares_column
+            break
+        else:
+            shares = None
 
-def is_adjustable_Korean_column(columns):
-    if "상장주식수" in columns:
-        return True
-    else:
-        return False
+    for volume_column in ['거래량', 'volume']:
+        if volume_column in columns:
+            volume = volume_column
+            break
+        else:
+            volume = None
 
-
-def is_adjustable_English_column(columns):
-    if "shares_outstanding" in columns:
-        return True
-    else:
-        return False
+    return shares, volume
 
 
 def adjust_price(dataframe: pd.DataFrame):
     # column has to be Korean column name
-    if is_adjustable_Korean_column(dataframe.columns):
-        shares = "상장주식수"
-        volume = "거래량"
-        available_column_list = ["종가", "대비", "시가", "고가", "저가", "거래량"]
-    elif is_adjustable_English_column(dataframe.columns):
-        shares = "shares_outstanding"
-        volume = "volume"
-        available_column_list = ["close", "change", "open", "high", "low", "volume"]
-    else:
+    shares, volume = get_shares_volume_column(dataframe.columns)
+
+    if shares is None or volume is None:
         warnings.warn("This data is not available to get adjusted price")
         return dataframe
 
     standard_ratio = dataframe[shares][0] / dataframe[shares]
+
+    available_column_list = ["close", "change", "open", "high", "low", "volume", "종가", "대비", "시가", "고가", "저가", "거래량"]
+
     for column in available_column_list:
         data = dataframe.get(column)
         if data is None:
@@ -69,23 +68,28 @@ def rename_dataframe(dataframe, column_file_name):
     return dataframe.rename(columns=new_column)
 
 
-def options(dataframe, **kwargs):
-    if isinstance(dataframe.columns, pd.core.indexes.multi.MultiIndex):
-        # multi index!!
-        # 어떻게 하지...?? 멀티인덱스는 기냥 따로 처리해줘야 하냐....
-        # 과감하게 어떠한 옵션도 주지 않겠다!!!
-        return dataframe
+def rename_dataframe_multi_index(dataframe, column_file_name):
+    new_column = get_column_map(column_file_name)
+    return dataframe.rename(columns=new_column)
 
+
+def options(dataframe, **kwargs):
     if kwargs.get("adjust") is True:
         dataframe = adjust_price(dataframe)
 
-    if kwargs.get("kor") is True:
-        # dataframe is coming with Korean columns so when kor is None, then if has to be changed
-        if not is_kor_column(dataframe.columns):
-            dataframe = rename_dataframe(dataframe, column_file_name="eng2kor")
+    if isinstance(dataframe.columns, pd.core.indexes.multi.MultiIndex):
+        if kwargs.get("kor") is True:
+            pass
+        else:
+            dataframe = rename_dataframe_multi_index(dataframe, column_file_name='kor2eng')
     else:
-        if is_kor_column(dataframe.columns):
-            dataframe = rename_dataframe(dataframe, column_file_name="kor2eng")
+        if kwargs.get("kor") is True:
+            # dataframe is coming with Korean columns so when kor is None, then if has to be changed
+            if not is_kor_column(dataframe.columns):
+                dataframe = rename_dataframe(dataframe, column_file_name="eng2kor")
+        else:
+            if is_kor_column(dataframe.columns):
+                dataframe = rename_dataframe(dataframe, column_file_name="kor2eng")
 
     if kwargs.get("reverse") is True:
         dataframe = dataframe.iloc[::-1]
